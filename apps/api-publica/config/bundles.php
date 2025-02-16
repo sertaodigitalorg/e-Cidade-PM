ad63d7afa4f8cb858efaf34e6d1c655966d15e62
Wellington Carvalho <wellington.cs@hotmail.com>
2025-02-16 14:54:02 -0300

    lib incluida na api public porem não reconhecida

--- apps/api-publica/config/bundles.php
+++ apps/api-publica/config/bundles.php
@@ -15,4 +15,7 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    ECidade\DataBaseLibrary\DataBaseLibraryBundle::class => ['all' => true],
 ];
