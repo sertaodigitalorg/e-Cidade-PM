49c504aae7a85473b26b3d8da91dcb6f3e88b6ed
Wellington Carvalho <wellington.cs@hotmail.com>
2025-02-21 19:35:39 -0300

    pages do login

--- frontends/auth-frontend/src/app/app.routes.ts
+++ frontends/auth-frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Route } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { NgModule } from '@angular/core';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: '', component: AppComponent },
+    { path: 'esqueci-senha', component: ForgotPasswordComponent },
+    { path: 'cadastrar', component: SignUpComponent }
+  ];
+
+  @NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutes {}
