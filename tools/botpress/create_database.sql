59b10d3f2e2ace55547e68d8ac284f5711feeb76
Wellington Carvalho <wellington.cs@hotmail.com>
2025-02-25 21:49:00 -0300

    configurações do bots

---
+++ tools/botpress/create_database.sql
@@ -0,0 +1,12 @@
+-- Database: botpress
+
+-- DROP DATABASE botpress;
+
+CREATE DATABASE botpress
+    WITH 
+    OWNER = ecidade
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
